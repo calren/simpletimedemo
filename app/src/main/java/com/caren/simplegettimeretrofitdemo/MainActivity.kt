989467efb460019d7c91be1e12e8da5c9b45334c
Caren Chang <carenc@google.com>
2020-12-14 15:28:39 -0800

    Add view model

--- app/src/main/java/com/caren/simplegettimeretrofitdemo/MainActivity.kt
+++ app/src/main/java/com/caren/simplegettimeretrofitdemo/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.caren.simplegettimeretrofitdemo
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,30 +23,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val BASE_URL = "http://worldclockapi.com/api/json/"
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service = retrofit.create(TimeApiEndpointInterface::class.java)
-
-        service.getTime().enqueue(object : Callback<TimeResponse> {
-            override fun onFailure(call: Call<TimeResponse>, t: Throwable) {
-                t.printStackTrace()
-                Log.e("Caren", "error calling time api")
-            }
-
-            override fun onResponse(call: Call<TimeResponse>, response: Response<TimeResponse>) {
-                val currentTime = response.body()?.currentDateTime
-                findViewById<TextView>(R.id.textView).text = currentTime
-                Log.i("Caren", "retrieved current time: " + currentTime)
-            }
-
-        })
+        // updates text view with time when response is received
+        viewModel.time.observe(this) {
+            Log.i("Caren", "live data updated")
+            findViewById<TextView>(R.id.textView).text = it.currentDateTime
+        }
     }
 }
--- app/src/main/java/com/caren/simplegettimeretrofitdemo/MainViewModel.kt
+++ app/src/main/java/com/caren/simplegettimeretrofitdemo/MainViewModel.kt
@@ -1,11 +1,44 @@
 package com.caren.simplegettimeretrofitdemo
 
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.*
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainViewModel() : ViewModel() {
+class MainViewModel : ViewModel() {
+
+    val _time: MutableLiveData<TimeResponse> = MutableLiveData()
+    val time: LiveData<TimeResponse> = _time
+
+    init {
+        val BASE_URL = "http://worldclockapi.com/api/json/"
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(TimeApiEndpointInterface::class.java)
+
+        service.getTime().enqueue(object : Callback<TimeResponse> {
+            override fun onFailure(call: Call<TimeResponse>, t: Throwable) {
+                t.printStackTrace()
+                Log.e("Caren", "error calling time api")
+            }
+
+            override fun onResponse(call: Call<TimeResponse>, response: Response<TimeResponse>) {
+                val currentTime = response.body()?.currentDateTime
+                _time.value = response.body()
+                Log.i("Caren", "retrieved current time: " + currentTime)
+            }
+
+        })
+    }
 
 }
